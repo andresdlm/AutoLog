a61ae37b0727c9e84d3472508b87e5c581c61b38
Andres Linarez <andreslinarez9@gmail.com>
2020-11-09 18:36:04 -0400

    Creado el StreamBuilder de Mantenimientos y su vista

---
+++ lib/Vehiculo/ui/widgets/cardMantenimiento.dart
@@ -0,0 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CardMantenimiento extends StatefulWidget {
+
+  final DocumentSnapshot documentSnapshot;
+  final VoidCallback onPressed;
+
+  CardMantenimiento({Key key, @required this.documentSnapshot, this.onPressed});
+
+  @override
+  _CardMantenimientoState createState() => _CardMantenimientoState(key: this.key, documentSnapshot: this.documentSnapshot, onPressed: this.onPressed);
+}
+
+class _CardMantenimientoState extends State<CardMantenimiento> {
+
+  final DocumentSnapshot documentSnapshot;
+  final VoidCallback onPressed;
+
+  _CardMantenimientoState({Key key, @required this.documentSnapshot, this.onPressed});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: widget.onPressed,
+      key: Key(documentSnapshot['modelo']),
+      child: Card(
+        elevation: 10,
+        margin: EdgeInsets.all(8),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        child: ListTile(
+          dense: true,
+          leading: Icon(Icons.car_repair),
+          title:Text(widget.documentSnapshot['tipoMantenimiento']),
+          subtitle:Text('Se realiza cada: ${documentSnapshot['frecuencia']} Km\nUltimo servicio a los: ${documentSnapshot['ultimoServicio']}'),
+          
+          trailing: Wrap(
+            spacing: -5,
+            children: <Widget>[
+              IconButton(
+                padding: const EdgeInsets.only(top: 0),
+                icon: Icon(
+                  Icons.settings,
+                  size: 35,
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+
+                },
+              ),
+              IconButton(
+                padding: const EdgeInsets.only(top: 0),
+                icon: Icon(
+                  Icons.delete,
+                  size: 35,
+                  color: Colors.red,
+                ),
+                onPressed: (){
+                  //userBloc.deleteVehiculo(documentSnapshot.id);
+                }
+              ),
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
--- lib/Vehiculo/ui/widgets/list-mantenimientos.dart
+++ lib/Vehiculo/ui/widgets/list-mantenimientos.dart
@@ -1,9 +1,27 @@
 import 'package:autolog/Vehiculo/ui/screens/add_mantenimiento.dart';
+import 'package:autolog/Vehiculo/ui/widgets/cardMantenimiento.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ListMantenimientos extends StatelessWidget {
+
+  String idVehiculo;  
+  final User user = FirebaseAuth.instance.currentUser;
+
+
+  ListMantenimientos({Key key, this.idVehiculo}): super(key: key) {
+    CollectionReference documentReference = FirebaseFirestore.instance.collection('Users').doc(user.uid).collection('Car');
+    Future<DocumentSnapshot> vehiculo = FirebaseFirestore.instance.collection('Users').doc(user.uid).collection('Car').doc(idVehiculo).get();
+    vehiculo.then((DocumentSnapshot carSnapshot) => {
+      
+    });
+  }
+
+  
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -13,6 +31,20 @@ class ListMantenimientos extends StatelessWidget {
         backgroundColor: Color(0xFF2196F3),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Users').doc(user.uid).collection('Car').doc(idVehiculo).collection("Mantenimientos").snapshots(),
+        builder: (context, snapshots) {
+          if(snapshots.data == null) return CircularProgressIndicator();
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: snapshots.data.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot documentSnapshot = snapshots.data.docs[index];
+              return CardMantenimiento(documentSnapshot: documentSnapshot);
+            },
+          );
+        },
+      ),
     );
   }
 }
